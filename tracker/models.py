4b1334ca73d331f1823818bcaa0c21023b360e8c
Erkenn <erkenkulbusinov@gmail.com>
2026-01-11 12:52:54 +0500

    модели данных

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    rawg_id = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    rawg_id = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True, null=True)
+    release_date = models.DateField(blank=True, null=True)
+    rating = models.FloatField(default=0)
+    metacritic = models.IntegerField(blank=True, null=True)
+    genres = models.ManyToManyField(Genre, related_name='games')
+    platforms = models.ManyToManyField(Platform, related_name='games')
+    image_url = models.URLField(blank=True, null=True)
+    rawg_id = models.IntegerField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-release_date']
